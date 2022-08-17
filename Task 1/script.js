a2448464f25516485f93f43b23cac7c457a3117e
Gytis <gg@gg.com>
2022-08-17 16:22:35 +0300

    Task 1 functionality done

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,3 +1,4 @@
+"use strict";
 /* ------------------------------ TASK 1 ----------------------------
 Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
 pamatyti jo pateikto svorio kovertavimą į:
@@ -8,3 +9,30 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+// konstantos
+const btnEl = document.getElementById("submit-btn");
+const outputEl = document.getElementById("output");
+const formEl = document.forms[0];
+
+// event listeners
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("mygtukas veikia, ir formoje gaunam: ", formEl.search.value);
+  //   send the input value to check if its a number
+  checkTheInput(+formEl.search.value);
+});
+
+function checkTheInput(value) {
+  console.log("type of input---", typeof value);
+  if (typeof value === "number") {
+    console.log("number is valid");
+    showTheOutput(value);
+  }
+}
+function showTheOutput(value) {
+  const kgToLb = value * 2.2046;
+  const kgToG = value * 1000;
+  const kgToOz = value * 35.274;
+  outputEl.innerHTML = `Your entered weight (${value}kg) translates to ${kgToLb} pounds, ${kgToG} grams and ${kgToOz} ounces`;
+}
